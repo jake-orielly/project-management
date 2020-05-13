14ac53c3e8103af11083075a3a67e9d4f9665d52
Jake O'Rielly <jake.orielly@gmail.com>
2020-05-13 08:23:42 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        currField: '',
+        fieldTypes: ['text','number','date','dropdown','slider'],
+        numDropdownFields: 0,
+        fields: []
+    },
+    methods: {
+        fieldAdd: function() {
+            let newField = {
+                label: document.getElementById("field-label").value,
+                type: document.getElementById("field-dropdown").value
+            }
+            
+            if (this.currField == 'dropdown') {
+                newField.options = [];
+                for (let i of document.getElementsByClassName("currDropdownOption"))
+                    newField.options.push(i.value);
+                this.numDropdownFields = 0;
+                
+            }
+            else if (this.currField == 'slider') {
+                newField.slider = {};
+                newField.slider.min = document.getElementById("slider-min").value;
+                newField.slider.max = document.getElementById("slider-max").value;
+                newField.slider.step = document.getElementById("slider-step").value;
+                newField.slider.initial = document.getElementById("slider-initial").value;
+            }
+
+            this.fields.push(newField);
+
+            document.getElementById("field-dropdown").value = "";
+            app.currField = "";
+            document.getElementById("field-label").value = "";
+        },
+        dropdownAdd: function() {
+            this.numDropdownFields++;
+        },
+        caps: function(text) {
+            if (!text)
+                return text;
+            return text[0].toUpperCase() + text.substr(1);
+        }
+    }
+})
