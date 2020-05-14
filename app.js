fb301f87200ce980dd704b23f89319e44d87be3b
Jake O'Rielly <jake.orielly@gmail.com>
2020-05-13 21:15:21 -0400

    Fleshing out dashboard ui

--- app.js
+++ app.js
@@ -5,15 +5,27 @@ var app = new Vue({
         currField: '',
         fieldTypes: ['text','number','date','dropdown','slider'],
         numDropdownFields: 0,
-        fields: []
+        fields: [],
+        currTab: "dashboard",
+        inbox: [
+            {
+                description: 'Give Estimate',
+                from: 'Ann Perkins',
+                arrived: new Date(2020, 5, 13)  
+            },
+            {
+                description: 'Confirm Completion',
+                from: 'Andy Dwyer',
+                arrived: new Date(2020, 5, 12)  
+            }
+        ]
     },
     methods: {
         fieldAdd: function() {
             let newField = {
                 label: document.getElementById("field-label").value,
                 type: document.getElementById("field-dropdown").value
-            }
-            
+            };
             if (this.currField == 'dropdown') {
                 newField.options = [];
                 for (let i of document.getElementsByClassName("currDropdownOption"))
@@ -34,6 +46,9 @@ var app = new Vue({
             document.getElementById("field-dropdown").value = "";
             app.currField = "";
             document.getElementById("field-label").value = "";
+        },
+        setTab: function(newTab) {
+            this.currTab = newTab;
         },
         dropdownAdd: function() {
             this.numDropdownFields++;
