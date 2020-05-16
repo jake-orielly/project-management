b75edb2fb925c17f171e4a11b2cbd9df8aea0e83
Jake O'Rielly <jake.orielly@gmail.com>
2020-05-16 20:35:51 +0000

    Flask can query forms from Mongodb

---
+++ flask/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask_cors import CORS
+from flask_restplus import Resource, Api
+import json
+from pymongo import MongoClient
+
+app = Flask(__name__) 
+api = Api(app)
+CORS(app)
+
+mongo_URL = "0.0.0.0:27017"
+
+@api.route('/is-alive')
+class isAlive(Resource):
+    def get(self):
+        return {'message': 'API is alive'}
+
+@api.route('/get-form/<name>')
+class GetForm(Resource):
+    def get(self, name):
+        client = MongoClient(mongo_URL)
+        db=client.project_management
+        collection = db[name]
+        cursor = collection.find({}, {'_id': False})
+        results = []
+        for i in cursor:
+            results.append(json.dumps(i))
+        return {"Message":results}
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',debug=True)
---
+++ js/requests.js
@@ -0,0 +1,10 @@
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", "http://vps:5000/get-form/annPerkinsForms", true);
+    xhttp.send();
+}
