787bb5eade357df45c62eda341cec3ca12c9d849
Jake O'Rielly <jake.orielly@gmail.com>
2020-05-16 16:56:37 -0400

    Added ability for frontend to query api

--- js/requests.js
+++ js/requests.js
@@ -1,10 +1,28 @@
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-        }
-    };
-    xhttp.open("GET", "http://vps:5000/get-form/annPerkinsForms", true);
-    xhttp.send();
+function getForm(form) {
+    return genericRequest("http://vps:5000/get-form/" + form)
+}
+
+function getUserForms(user) {
+    return genericRequest("http://vps:5000/get-user-forms/" + user)
+}
+
+function genericRequest(url) {
+    var request = new XMLHttpRequest();
+
+    return new Promise(function (resolve, reject) {
+		request.onreadystatechange = function() {
+            if (request.readyState !== 4) return;
+            
+			if (request.status >= 200 && request.status < 300) {
+				resolve(request);
+			} else {
+				reject({
+					status: request.status,
+					statusText: request.statusText
+				});
+			}
+        };
+        request.open("GET", url, true);
+        request.send();
+	});
 }
