9406643223f6b96af2afffda5a551ccaffe123ea
Jake O'Rielly <jake.orielly@gmail.com>
2020-05-17 15:56:34 +0000

    Merge branch 'master' of https://github.com/jake-orielly/project-management

--- js/requests.js
+++ js/requests.js
@@ -1,10 +1,38 @@
-function loadDoc() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-        }
-    };
-    xhttp.open("GET", "http://vps:5000/get-form/annPerkinsForms", true);
-    xhttp.send();
+function getForm(form) {
+    return genericRequest("http://vps:5000/get-form/" + form, "GET")
+}
+
+function getUserForms(user) {
+    return genericRequest("http://vps:5000/get-user-forms/" + user, "GET")
+}
+
+function login(username,password) {
+	return genericRequest("http://vps:5000/login", "POST",
+		{
+			"user":username,
+			"password":password
+		}
+	)
+}
+
+
+function genericRequest(url,type,body) {
+    var request = new XMLHttpRequest();
+
+    return new Promise(function (resolve, reject) {
+		request.onreadystatechange = function() {
+            if (request.readyState !== 4) return;
+            
+			if (request.status >= 200 && request.status < 300) {
+				resolve(request);
+			} else {
+				reject({
+					status: request.status,
+					statusText: request.statusText
+				});
+			}
+        };
+        request.open(type, url, true);
+        request.send(body);
+	});
 }
