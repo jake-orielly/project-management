91341dc1bcc245627e78a0a6870c9b206a225e21
Jake O'Rielly <jake.orielly@gmail.com>
2020-06-05 02:13:07 +0000

    Little bit of cleanup for login page

--- frontend/public/app.vue
+++ frontend/public/app.vue
@@ -2,9 +2,9 @@
     <div id="app">
         <div id="login-container">
             <p>Login</p>
-            <input type="text" autocomplete="off" placeholder="Username" id="username">
+            <input type="text" autocomplete="off" placeholder="Username" v-model="username" id="username">
             <br>
-            <input type="password" autocomplete="off" placeholder="Password" id="password">
+            <input type="password" autocomplete="off" placeholder="Password" v-model="password" id="password">
             <br>
             <button @click="login">Log In</button>
             <p :class="[(loginStatus == 'failed' ? 'error' : '')]" id="login-status-text">{{loginStatusText}}</p>
@@ -23,6 +23,8 @@
         data () {
             return {
                 loginStatus: "",
+                username:"",
+                password:""
             }
         },
         computed: {
@@ -35,17 +37,13 @@
         },
         methods: {
             login() {
-                let username = document.getElementById("username").value;
-                let password = document.getElementById("password").value;
-
-                requests.postLogin(username,password).then(
+                requests.postLogin(this.username,password).then(
                     response => {
                         let result = JSON.parse(response.responseText).message;
-                        console.log(result)
                         if (result == "success") {
-                            if (username == "ann.perkins")
+                            if (this.username == "ann.perkins")
                                 window.location.href = "./dashboard.html";
-                            else if (username == "leslie.knope")
+                            else if (this.username == "leslie.knope")
                                 window.location.href = "./my-team.html";
                         }
                         else
