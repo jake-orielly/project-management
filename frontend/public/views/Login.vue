9684f5bae19ab63cf3d10349080b8e0c44e6e0bd
Jake O'Rielly <jake.orielly@gmail.com>
2020-06-06 21:56:11 +0000

    Login styling

--- frontend/public/views/Login.vue
+++ frontend/public/views/Login.vue
@@ -1,17 +1,18 @@
 <template>
     <div id="login-container">
-        <p>Login</p>
-        <input type="text" autocomplete="off" placeholder="Username" v-model="username" id="username">
-        <br>
-        <input type="password" autocomplete="off" placeholder="Password" v-model="password" id="password">
-        <br>
-        <button @click="login">Log In</button>
-        <p :class="[(loginStatus == 'failed' ? 'error' : '')]" id="login-status-text">{{loginStatusText}}</p>
+        <div id="login-inner">
+            <p>Login</p>
+            <input type="text" autocomplete="off" placeholder="Username" v-model="username" id="username">
+            <br>
+            <input type="password" autocomplete="off" placeholder="Password" v-model="password" id="password">
+            <br>
+            <button @click="login">Log In</button>
+            <p :class="[(loginStatus == 'failed' ? 'error' : '')]" id="login-status-text">{{loginStatusText}}</p>
+        </div>
     </div>
 </template>
 
 <script>
-    import '../css/login.css'
     import requests from '../services/requests.js';
     export default {
         data () {
@@ -48,3 +49,43 @@
         }
     }
 </script>
+
+<style lang="scss" scoped>
+    #login-container {
+        text-align: center;
+    }
+
+    $primary-color: #2C666E;
+
+    input {
+        margin: 0.5em;
+    }
+
+    .error {
+        color: #d91e27;
+    }
+
+    #login-inner {
+        display: inline-block;
+        border: 4px solid $primary-color;
+        margin-top: 15%;
+        border-radius: 1em;
+        padding: 3em 1em 0;
+
+        p {
+            text-decoration: underline;
+        }
+
+        button {
+            margin-top: 2rem;
+        }
+    }
+
+    #login-container, #login-container * {
+        font-size: 1.5rem;
+    }
+
+    #login-status-text {
+        min-height: 1.5rem;
+    }
+</style>
