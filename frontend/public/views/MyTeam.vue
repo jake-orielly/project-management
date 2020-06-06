e90a93cedb5b62d65385c90afc1a6ae50f976321
Jake O'Rielly <jake.orielly@gmail.com>
2020-06-06 20:34:55 +0000

    Importing font awesome

--- frontend/public/views/MyTeam.vue
+++ frontend/public/views/MyTeam.vue
@@ -37,6 +37,10 @@
 
 <script>
     import '../css/style.css'
+
+    import '@fortawesome/fontawesome-free/css/all.css'
+    import '@fortawesome/fontawesome-free/js/all.js'
+
     import requests from '../services/requests.js';
     import Banner from '../components/Banner.vue';
 
