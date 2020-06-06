8c8c68e0d619d5ac5295d10eb8ea9d2261a5e487
Jake O'Rielly <jake.orielly@gmail.com>
2020-06-06 17:09:42 +0000

    Vue router working

---
+++ frontend/public/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <router-view/>
+    </div>
+</template>
+
+<script src="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet"></script>
+<script type="text/javascript" src="./js/config.js"></script>
+
+<script>
+    import './css/style.css'
+    import Login from './components/login.vue';
+    import Foo from './components/foo.vue';
+
+    import Vue from 'vue'
+    import VueRouter from 'vue-router'
+
+    Vue.use(VueRouter)
+
+    const routes = [
+        { path: '/', component: Login },
+        { path: '/foo', component: Foo }
+    ];
+
+    const router = new VueRouter({routes});
+    export default {
+        name: 'app',
+        router,
+        components: {Login,Foo},
+    };
+</script>
