4a32ef7bc62570cd3c67f36d58a9b27f4d58cffc
Jake O'Rielly <jake.orielly@gmail.com>
2020-08-16 21:43:55 +0000

    Team members display for Team Management

--- frontend/public/views/TeamManagement.vue
+++ frontend/public/views/TeamManagement.vue
@@ -2,6 +2,13 @@
 <template>
     <div>
         <Banner></Banner>
+        <div id="main-container">
+            <p class="title">My Team</p>
+            <p v-for="user in myTeam">
+                {{user.name}}
+                <i class="fa fa-user-minus"></i>
+            </p>
+        </div>
     </div>
 </template>
 
@@ -20,9 +27,19 @@
         },
         data () {
             return {
+                myTeam:[]
             }
         },
         mounted () {
+            requests.getUser(this.$store.state.user).then(
+                response => {
+                    for (let i of JSON.parse(response.responseText).team)
+                        this.myTeam.push({
+                            name:i,
+                            open:false
+                        });
+                }
+            )
         },
         methods: {
         }
@@ -30,4 +47,12 @@
 </script>
 
 <style lang="scss" scoped>
+    #main-container {
+        padding: 2rem;
+    }
+
+    .title {
+        font-weight: bold;
+        text-decoration: underline;
+    }
 </style>
